8b603421e5619ed958b0f9359610168fb69b2f91
KaranGauswami <karangauswami.dev@gmail.com>
2020-10-29 18:45:07 +0530

    Created separated files and changed methods from owned to borrowed.

--- src/helper.rs
+++ src/helper.rs
@@ -1,26 +1,22 @@
 use crate::Bigbluebutton;
 use sha2::{Digest, Sha256};
-use std::collections::HashMap;
 
-impl Bigbluebutton {
+impl Bigbluebutton<'_> {
     /// hash function for converting to SHA-256
-    pub(crate) fn hash(payload: Vec<String>) -> String {
+    pub(crate) fn hash(payload: Vec<&str>) -> String {
         let mut hasher = Sha256::new();
-
         hasher.update(payload.join(""));
         let result = hasher.finalize();
         let hash_value = result.as_slice();
-
         hex::encode(hash_value)
     }
 
     /// serialize query parameters into query string
-    pub(crate) fn serialize_params(params: HashMap<String, String>) -> String {
-        let mut key_value = Vec::new();
-        for (key, value) in params {
-            key_value.push(format!("{}={}", key, value));
-        }
-        let serialized = key_value.join("&");
-        serialized
+    pub(crate) fn serialize_params(params: Vec<(&str, &str)>) -> String {
+        let collection: Vec<String> = params
+            .iter()
+            .map(|(key, value)| format!("{}={}", key, value))
+            .collect();
+        collection.join("&").to_string()
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,69 @@
-use std::collections::HashMap;
-
+#![deny(missing_docs, rust_2018_idioms)]
+//! BigBlueButton is an open source web conferencing system for online learning.
+//!
+//! This crate provides interface for interacting with Bigbluebutton APIs.
+//!
+//! More details can be found [here](https://docs.bigbluebutton.org/dev/api.html)
+//! # Examples
+//!```rust
+//! # use bigbluebutton::Bigbluebutton;
+//!
+//! // Creates new BBB Instance
+//! let bbb = Bigbluebutton::new(
+//!     "https://example.com/bigbluebutton/api/",
+//!     "BBBSECRET",
+//!  );
+//!
+//! let params = vec![
+//!     ("password", "pass"),
+//!     ("fullName", "name"),
+//!     ("meetingId", "1"),
+//! ];
+//!
+//! let url = bbb.generate_url("join", params);
+//! # assert_eq!(url,"https://example.com/bigbluebutton/api/join?password=pass&fullName=name&meetingId=1&checksum=94e467c1b4b13f4452ca5d1deb9b7b74e1063aea55fe078139015a7d6311cfdf");
+//! println!("{}",url) // https://example.com/bigbluebutton/api/join?password=pass&fullName=name&meetingId=1&checksum=94e467c1b4b13f4452ca5d1deb9b7b74e1063aea55fe078139015a7d6311cfdf
+//!```
 mod helper;
+// mod meeting;
 
-pub struct Bigbluebutton {
-    salt: String,
-    url: String,
+/// Implementation of Bigbluebutton APIs
+pub struct Bigbluebutton<'a> {
+    salt: &'a str,
+    url: &'a str,
 }
-impl Bigbluebutton {
+impl<'a> Bigbluebutton<'a> {
     /// creates new BBB API Client
-    pub fn new(url: String, salt: String) -> Self {
-        let mut api_url = url;
-        api_url.push_str("api/");
-        Self { salt, url: api_url }
+    pub fn new(url: &'a str, salt: &'a str) -> Self {
+        Self { salt, url }
     }
 
-    /// Generates BBB URL
-    pub fn generate_url(self, action: String, params: HashMap<String, String>) -> String {
+    /// Generates BBB URL with checksum to interact with BBB server
+    pub fn generate_url(self, action: &str, params: Vec<(&str, &str)>) -> String {
         let query_params = self::Bigbluebutton::serialize_params(params);
-        let checksum =
-            self::Bigbluebutton::hash(vec![action.clone(), query_params.clone(), self.salt]);
+        let checksum = self::Bigbluebutton::hash(vec![action, &query_params, self.salt]);
         format!(
             "{}{}?{}&checksum={}",
             self.url, action, query_params, checksum
         )
     }
+}
 
-    /// Creates a BigBlueButton meeting.
-    ///
-    /// The create call is idempotent: you can call it multiple times with the same parameters
-    /// without side effects.
-    /// This simplifies the logic for joining a user into a session as your application can always
-    /// call create before returning the join URL to the user. This way, regardless of the order in
-    /// which users join, the meeting will always exist when the user tries to join (the first create
-    /// call actually creates the meeting; subsequent calls to create simply return SUCCESS).
-    /// The BigBlueButton server will automatically remove empty meetings that were created but have
-    /// never had any users after a number of minutes specified by meetingExpireIfNoUserJoinedInMinutes
-    /// defined in bigbluebutton.properties.
-    pub fn create(self, params: HashMap<String, String>) {
-        unimplemented!();
+#[cfg(test)]
+mod test {
+    use super::Bigbluebutton;
+
+    #[test]
+    fn generate_url() {
+        let bbb = Bigbluebutton::new("https://example.com/bigbluebutton/api/", "yourbbbsecret");
+
+        let params = vec![
+            ("password", "yourpassword"),
+            ("fullName", "fullname"),
+            ("meetingID", "127"),
+        ];
+
+        let url = bbb.generate_url("join", params);
+        assert_eq!(url,"https://example.com/bigbluebutton/api/join?password=yourpassword&fullName=fullname&meetingID=127&checksum=40799bd174680c4613a512cb000eee0bb9beafe1bcb1d380b23c0720e5d0f609");
     }
 }
