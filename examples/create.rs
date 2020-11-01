6d476425c0b1cf6a72c8f1ae3400d1a712175dda
KaranGauswami <karangauswami.dev@gmail.com>
2020-11-01 10:29:02 +0530

    fixes test

--- examples/create.rs
+++ examples/create.rs
@@ -4,11 +4,20 @@ use bigbluebutton::{Bigbluebutton, Execute};
 
 #[tokio::main]
 async fn main() {
-    let bbb = Bigbluebutton::new("https://example.com/bigbluebutton/", "secret");
+    let bbb_url = std::env::var("BBB_URL").expect("BBB_URL is not set");
+    let bbb_secret = std::env::var("BBB_SECRET").expect("BBB_SECRET is not set");
+    let bbb = Bigbluebutton::new(&bbb_url, &bbb_secret);
     let mut request = CreateMeetingRequest::new();
     request.meeting_id = Some("12".to_string());
     request.moderator_pw = Some("modp".to_string());
-    request.attandee_pw = Some("akarr".to_string());
+    request.attendee_pw = Some("akarr".to_string());
 
     let _response = bbb.execute(&request).await;
+
+    let mut request = bigbluebutton::administration::EndMeetingRequest::new();
+    request.meeting_id = Some("12".to_string());
+    request.password = Some("modp".to_string());
+
+    let response = bbb.execute(&request).await;
+    println!("{:?}", response);
 }
