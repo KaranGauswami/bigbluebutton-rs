08a908f53a11cfa193355048858aa2a7fb391894
Karan Gauswami <karangauswami.dev@gmail.com>
2021-11-21 14:13:16 +0530

    refactored tests to use tokio::test macro

--- tests/administration.rs
+++ tests/administration.rs
@@ -4,68 +4,62 @@ mod test {
     use bigbluebutton::{Bigbluebutton, Execute};
     use std::env::var;
 
-    #[test]
+    #[tokio::test]
     #[ignore]
-    fn create_meeting() {
+    async fn create_meeting() {
         let bbb_url = var("BBB_URL").expect("BBB_URL is not set");
         let bbb_secret = var("BBB_SECRET").expect("BBB_SECRET is not set");
         let bbb = Bigbluebutton::new(&bbb_url, &bbb_secret);
 
-        let rt = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        rt.block_on(async {
-            let meeting_id = "1".to_string();
-            let attendee_pw = "attendeep".to_string();
-            let moderator_pw = "modp".to_string();
-            let voice_bridge = "70757".to_string();
-            let dial_number = "70757".to_string();
-            let duration = 0;
+        let meeting_id = "1".to_string();
+        let attendee_pw = "attendeep".to_string();
+        let moderator_pw = "modp".to_string();
+        let voice_bridge = "70757".to_string();
+        let dial_number = "70757".to_string();
+        let duration = 0;
 
-            let mut request = CreateMeetingRequest::new(&meeting_id);
+        let mut request = CreateMeetingRequest::new(&meeting_id);
 
-            request.attendee_pw = Some(attendee_pw.clone());
-            request.moderator_pw = Some(moderator_pw.clone());
-            request.voice_bridge = Some(voice_bridge.clone());
-            request.dial_number = Some(dial_number.clone());
-            request.duration = Some(duration.clone());
+        request.attendee_pw = Some(attendee_pw.clone());
+        request.moderator_pw = Some(moderator_pw.clone());
+        request.voice_bridge = Some(voice_bridge.clone());
+        request.dial_number = Some(dial_number.clone());
+        request.duration = Some(duration.clone());
 
-            let response = bbb
-                .execute(&request)
-                .await
-                .expect("Unable to parse CreateMeetingResponse");
-            assert_eq!(response.meeting_id(), &meeting_id);
-            assert_eq!(response.attendee_pw(), &attendee_pw);
-            assert_eq!(response.moderator_pw(), &moderator_pw);
-            assert_eq!(response.voice_bridge(), &voice_bridge);
-            assert_eq!(response.dial_number(), &dial_number);
-            assert_eq!(response.duration(), &duration);
-        });
+        let response = bbb
+            .execute(&request)
+            .await
+            .expect("Unable to parse CreateMeetingResponse");
+        assert_eq!(response.meeting_id(), &meeting_id);
+        assert_eq!(response.attendee_pw(), &attendee_pw);
+        assert_eq!(response.moderator_pw(), &moderator_pw);
+        assert_eq!(response.voice_bridge(), &voice_bridge);
+        assert_eq!(response.dial_number(), &dial_number);
+        assert_eq!(response.duration(), &duration);
     }
 
-    #[test]
+    #[tokio::test]
     #[ignore]
-    fn end_meeting() {
+    async fn end_meeting() {
         let bbb_url = var("BBB_URL").expect("BBB_URL is not set");
         let bbb_secret = var("BBB_SECRET").expect("BBB_SECRET is not set");
         let bbb = Bigbluebutton::new(&bbb_url, &bbb_secret);
 
-        let rt = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        rt.block_on(async {
-            let mut req = CreateMeetingRequest::new("2");
-            req.moderator_pw = Some("modp".to_string());
-            let _ = bbb.execute(&req).await;
+        let mut req = CreateMeetingRequest::new("2");
+        req.moderator_pw = Some("modp".to_string());
+        let _ = bbb.execute(&req).await;
 
-            let req = EndMeetingRequest::new("2", "modp");
+        let req = EndMeetingRequest::new("2", "modp");
 
-            let response = bbb
-                .execute(&req)
-                .await
-                .expect("Unable to parse EndMeetingResponse");
-            println!("{:?}", response);
-            assert_eq!(
-                response.return_code,
-                bigbluebutton::error::ResponseCode::SUCCESS
-            );
-            assert_eq!(response.message_key, "sentEndMeetingRequest".to_string());
-        })
+        let response = bbb
+            .execute(&req)
+            .await
+            .expect("Unable to parse EndMeetingResponse");
+        println!("{:?}", response);
+        assert_eq!(
+            response.return_code,
+            bigbluebutton::error::ResponseCode::SUCCESS
+        );
+        assert_eq!(response.message_key, "sentEndMeetingRequest".to_string());
     }
 }
--- tests/monitoring.rs
+++ tests/monitoring.rs
@@ -4,59 +4,53 @@ mod test {
     use bigbluebutton::monitoring::{GetMeetingInfoRequest, GetMeetingsRequest};
     use bigbluebutton::{Bigbluebutton, Execute};
 
-    #[test]
+    #[tokio::test]
     #[ignore]
-    fn get_meeting_info() {
+    async fn get_meeting_info() {
         let bbb_url = std::env::var("BBB_URL").expect("BBB_URL is not set");
         let bbb_secret = std::env::var("BBB_SECRET").expect("BBB_SECRET is not set");
         let bbb = Bigbluebutton::new(&bbb_url, &bbb_secret);
 
-        let rt = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        rt.block_on(async {
-            let mut request = CreateMeetingRequest::new("14");
-            request.moderator_pw = Some("modp".to_string());
-            request.attendee_pw = Some("attep".to_string());
+        let mut request = CreateMeetingRequest::new("14");
+        request.moderator_pw = Some("modp".to_string());
+        request.attendee_pw = Some("attep".to_string());
 
-            let _ = bbb.execute(&request).await;
+        let _ = bbb.execute(&request).await;
 
-            let mut request = GetMeetingInfoRequest::new();
-            request.meeting_id = Some("14".to_string());
+        let mut request = GetMeetingInfoRequest::new();
+        request.meeting_id = Some("14".to_string());
 
-            let response = bbb
-                .execute(&request)
-                .await
-                .expect("Unable to parse GetMeetingInfoResponse");
-            assert_eq!(response.meeting_id(), &"14".to_string());
-        });
+        let response = bbb
+            .execute(&request)
+            .await
+            .expect("Unable to parse GetMeetingInfoResponse");
+        assert_eq!(response.meeting_id(), &"14".to_string());
     }
 
-    #[test]
+    #[tokio::test]
     #[ignore]
-    fn get_meetings() {
+    async fn get_meetings() {
         let bbb_url = std::env::var("BBB_URL").expect("BBB_URL is not set");
         let bbb_secret = std::env::var("BBB_SECRET").expect("BBB_SECRET is not set");
         let bbb = Bigbluebutton::new(&bbb_url, &bbb_secret);
 
-        let rt = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
-        rt.block_on(async {
-            let mut request = CreateMeetingRequest::new("15");
-            request.moderator_pw = Some("modp".to_string());
-            request.attendee_pw = Some("attep".to_string());
+        let mut request = CreateMeetingRequest::new("15");
+        request.moderator_pw = Some("modp".to_string());
+        request.attendee_pw = Some("attep".to_string());
 
-            let mut request2 = CreateMeetingRequest::new("16");
-            request2.moderator_pw = Some("modp".to_string());
-            request2.attendee_pw = Some("attep".to_string());
+        let mut request2 = CreateMeetingRequest::new("16");
+        request2.moderator_pw = Some("modp".to_string());
+        request2.attendee_pw = Some("attep".to_string());
 
-            let _ = bbb.execute(&request).await;
-            let _ = bbb.execute(&request2).await;
+        let _ = bbb.execute(&request).await;
+        let _ = bbb.execute(&request2).await;
 
-            let request = GetMeetingsRequest::new();
+        let request = GetMeetingsRequest::new();
 
-            let response = bbb
-                .execute(&request)
-                .await
-                .expect("Unable to parse GetMeetingsResponse");
-            assert_ne!(response.meetings().len(), 0);
-        });
+        let response = bbb
+            .execute(&request)
+            .await
+            .expect("Unable to parse GetMeetingsResponse");
+        assert_ne!(response.meetings().len(), 0);
     }
 }
